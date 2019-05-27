cec4b01e737d4436d78325588d536552f84b6430
Scott Giminiani <scottg489@gmail.com>
2019-05-27 16:19:26 -0700

    Add Dockerfile and required files

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:18.10
+
+RUN apt-get update
+RUN apt-get install -y git docker.io jq
+
+RUN mkdir /root/.ssh
+COPY known_hosts /root/.ssh/known_hosts
+
+RUN mkdir /root/.docker
+
+RUN mkdir -p /opt/build
+COPY build.sh /opt/build/build.sh
+
+WORKDIR /opt/build
+ENTRYPOINT ["./build.sh"]
---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+ID_RSA_CONTENTS=$(echo -n $1 | jq -r .ID_RSA | base64 --decode)
+DOCKER_CONFIG_CONTENTS=$(echo -n $1 | jq -r .DOCKER_CONFIG | base64 --decode)
+
+GIT_REPO_URL=$(echo -n $1 | jq -r .GIT_REPO_URL | base64 --decode)
+RELATIVE_SUB_DIR=$(echo -n $1 | jq -r .RELATIVE_SUB_DIR | base64 --decode)
+DOCKER_IMAGE_NAME=$(echo -n $1 | jq -r .DOCKER_IMAGE_NAME | base64 --decode)
+
+printf -- "$ID_RSA_CONTENTS" > /root/.ssh/id_rsa
+printf -- "$DOCKER_CONFIG_CONTENTS" > /root/.docker/config.json
+
+chmod 400 /root/.ssh/id_rsa
+
+git clone $GIT_REPO_URL repo
+cd repo/$RELATIVE_SUB_DIR
+
+docker build -t $DOCKER_IMAGE_NAME .
+docker push $DOCKER_IMAGE_NAME
