f03d9937c25618c73bfd23cfd3ced027a631821f
Scott Giminiani <scottg489@gmail.com>
2019-05-27 17:45:10 -0700

    Don't try to base64 decode git repo, sub dir, and docker image name
    These values are meant to be passed in plain text

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ echo $1
 ID_RSA_CONTENTS=$(echo -n $1 | jq -r .ID_RSA | base64 --decode)
 DOCKER_CONFIG_CONTENTS=$(echo -n $1 | jq -r .DOCKER_CONFIG | base64 --decode)
 
-GIT_REPO_URL=$(echo -n $1 | jq -r .GIT_REPO_URL | base64 --decode)
-RELATIVE_SUB_DIR=$(echo -n $1 | jq -r .RELATIVE_SUB_DIR | base64 --decode)
-DOCKER_IMAGE_NAME=$(echo -n $1 | jq -r .DOCKER_IMAGE_NAME | base64 --decode)
+GIT_REPO_URL=$(echo -n $1 | jq -r .GIT_REPO_URL)
+RELATIVE_SUB_DIR=$(echo -n $1 | jq -r .RELATIVE_SUB_DIR)
+DOCKER_IMAGE_NAME=$(echo -n $1 | jq -r .DOCKER_IMAGE_NAME)
 
 printf -- "$ID_RSA_CONTENTS" > /root/.ssh/id_rsa
 printf -- "$DOCKER_CONFIG_CONTENTS" > /root/.docker/config.json
