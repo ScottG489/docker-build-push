eb0c31269e3d4cb47e41a0c6b9e6abd3f2a32876
Scott Giminiani <scottg489@gmail.com>
2020-06-14 20:35:46 -0700

    Use new URL for build server

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "GIT_REPO_URL": "'"$GIT_REPO_URL"'", "RELATIVE_SUB_DIR": "'"$RELATIVE_SUB_DIR"'", "DOCKER_IMAGE_NAME": "'"$DOCKER_IMAGE_NAME"'"}' 'https://diff-data.com/build?image=scottg489/docker-build-push:latest'
+curl -v -s --data-binary '{"ID_RSA": "'"$1"'", "DOCKER_CONFIG": "'"$2"'", "GIT_REPO_URL": "'"$GIT_REPO_URL"'", "RELATIVE_SUB_DIR": "'"$RELATIVE_SUB_DIR"'", "DOCKER_IMAGE_NAME": "'"$DOCKER_IMAGE_NAME"'"}' 'http://simple-ci.com:8080/build?image=scottg489/docker-build-push:latest'
