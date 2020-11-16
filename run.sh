80267e522e544c7703155ae34f269672610d62de
Scott Giminiani <scottg489@gmail.com>
2020-11-16 18:16:06 -0500

    Run docker daemon so we can run docker
    Since the new conjob build server doesn't mount docker.sock but instead
    uses the sysbox runtime, we need to run docker ourselves.

--- run.sh
+++ run.sh
@@ -11,6 +11,12 @@ DOCKER_IMAGE_NAME=$(echo -n $1 | jq -r .DOCKER_IMAGE_NAME)
 printf -- "$ID_RSA_CONTENTS" > /root/.ssh/id_rsa
 printf -- "$DOCKER_CONFIG_CONTENTS" > /root/.docker/config.json
 
+set -x
+
+# Start the docker daemon. This is necessary when using the sysbox-runc container runtime rather than moutning docker.sock
+dockerd > /var/log/dockerd.log 2>&1 &
+sleep 3
+
 chmod 400 /root/.ssh/id_rsa
 
 git clone $GIT_REPO_URL repo
