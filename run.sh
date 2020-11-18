917ebcfe01122ff86232b223d472099b8f2bf06f
Scott Giminiani <scottg489@gmail.com>
2020-11-17 23:39:04 -0500

    Move part of secret setup before logging

--- run.sh
+++ run.sh
@@ -11,13 +11,13 @@ DOCKER_IMAGE_NAME=$(echo -n $1 | jq -r .DOCKER_IMAGE_NAME)
 printf -- "$ID_RSA_CONTENTS" > /root/.ssh/id_rsa
 printf -- "$DOCKER_CONFIG_CONTENTS" > /root/.docker/config.json
 
+chmod 400 /root/.ssh/id_rsa
+
 set -x
 
 # Start the docker daemon. This is necessary when using the sysbox-runc container runtime rather than moutning docker.sock
 dockerd > /var/log/dockerd.log 2>&1 &
 sleep 3
-
-chmod 400 /root/.ssh/id_rsa
 
 git clone $GIT_REPO_URL repo
 cd repo/$RELATIVE_SUB_DIR
