ed3d6eea2a7d61f761fe5bdaf31c286a246b1fce
Scott Giminiani <scottg489@gmail.com>
2025-12-02 18:41:13 -0500

    Specify deploy build task
    Even though we default to this, for our own build lets explicity specify
    it.

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 readonly IMAGE_NAME='scottg489/docker-build-push:latest'
-readonly ID_RSA=$1
-readonly DOCKER_CONFIG=$2
+readonly RUN_TASK=$1
+readonly ID_RSA=$2
+readonly DOCKER_CONFIG=$3
 
 read -r -d '' JSON_BODY <<- EOM
   {
+  "RUN_TASK": "$RUN_TASK",
   "ID_RSA": "$ID_RSA",
   "DOCKER_CONFIG": "$DOCKER_CONFIG",
   "GIT_REPO_URL": "$GIT_REPO_URL",
